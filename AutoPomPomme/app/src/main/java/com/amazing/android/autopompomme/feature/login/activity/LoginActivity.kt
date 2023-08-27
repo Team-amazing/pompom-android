5c9827f756461f527d068d5e07a3051fc5a79f07
parkUiery <parkuiery@gmail.com>
2023-08-27 16:20:15 +0900

    ♻ :: 코드 수정

--- AutoPomPomme/app/src/main/java/com/amazing/android/autopompomme/feature/login/activity/LoginActivity.kt
+++ AutoPomPomme/app/src/main/java/com/amazing/android/autopompomme/feature/login/activity/LoginActivity.kt
@@ -1,61 +1,52 @@
 package com.amazing.android.autopompomme.feature.login.activity
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.amazing.android.autopompomme.databinding.ActivityLoginBinding
+import com.amazing.android.autopompomme.feature.login.repository.LoginRepository
+import com.amazing.android.autopompomme.feature.login.viewmodel.LoginViewModel
+import com.amazing.android.autopompomme.feature.login.viewmodel.factory.LoginViewModelFactory
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
-    private var mBinding: ActivityLoginBinding? = null
-    private val binding get() = mBinding!!
+    lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var viewModel: LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        mBinding = ActivityLoginBinding.inflate(layoutInflater)
-
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        val repository = LoginRepository(auth)
+        val factory = LoginViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, factory).get(LoginViewModel::class.java)
+
         binding.btnLoginLogin.setOnClickListener {
-            login()
-        }
-    }
+            val email = binding.etLoginId.text.toString()
+            val password = binding.etLoginPassword.text.toString()
+            if (binding.etLoginId.length() > 0 || binding.etLoginPassword.length() > 0) {
+                binding.btnLoginLogin.isEnabled = true
 
-    private fun login() {
-        if (binding.etLoginId.length() > 0 || binding.etLoginPassword.length() > 0) {
-            binding.btnLoginLogin.isEnabled = true
-            sever()
-        }
-
-    }
-
-    private fun sever() {
-        val email = binding.etLoginId
-        val password = binding.etLoginPassword
-
-        auth.signInWithEmailAndPassword(email.toString(), password.toString())
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    val user = auth.currentUser
-                    //updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Toast.makeText(
-                        baseContext,
-                        "Authentication failed.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                    //updateUI(null)
-                }
             }
+            viewModel.login(email, password)
+
+        }
+
+        observeViewModel()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-        mBinding = null
+    private fun observeViewModel() {
+        viewModel.loginResult.observe(this) { result ->
+            if (result) {
+                //로그인 성공
+            } else {
+                //로그인 실패
+            }
+        }
     }
 }
 
