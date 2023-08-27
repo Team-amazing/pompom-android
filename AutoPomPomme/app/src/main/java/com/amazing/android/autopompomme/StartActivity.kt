ed2a08ac73592e0e3816ca8dd712890c9ad82786
parkUiery <parkuiery@gmail.com>
2023-08-27 13:31:36 +0900

    ♻ :: 패키지 변경

--- AutoPomPomme/app/src/main/java/com/amazing/android/autopompomme/StartActivity.kt
+++ AutoPomPomme/app/src/main/java/com/amazing/android/autopompomme/StartActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.amazing.android.autopompomme.databinding.ActivityStartBinding
+import com.amazing.android.autopompomme.feature.login.Activity.LoginActivity
 
 class StartActivity : AppCompatActivity() {
 
